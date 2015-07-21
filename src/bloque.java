b6f107c621610c0f9dadf99f5bbbc4d0205c02a8
ShunTr-LabGames <repaco9@gmail.com>
2015-07-21 18:19:38 +0200

    arcanoid java

---
+++ src/bloque.java
@@ -0,0 +1,17 @@
+
+public class bloque {
+    public int x;
+    public int y;
+    public int alto;
+    public int ancho;
+    public boolean destruido;
+   
+    public bloque(int x,int y, int alto, int ancho, boolean destruido) {
+        this.x = x;
+        this.y = y;
+        this.alto = alto;
+        this.ancho = ancho;
+        this.destruido = destruido;
+    }
+
+}
---
+++ src/pelota.java
@@ -0,0 +1,15 @@
+public class pelota {
+    public int x;
+    public int y;
+    public int veloX;
+    public int veloY;
+    public int ladoPelota;
+   
+    public pelota(int x, int y, int veloX, int veloY, int ladoPelota) {
+        this.x = x;
+        this.y = y; 
+        this.veloX = veloX;
+        this.veloY = veloY;
+        this.ladoPelota= ladoPelota;
+    }
+}
