0e763eaea461d202bb231395b5372bc91f4353a5
psetta <manuel.mayo.lado@gmail.com>
2019-03-02 06:45:58 +0100

    auto

--- src/main.java
+++ src/main.java
@@ -1,5 +1,4 @@
 import java.awt.Color;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
@@ -12,27 +11,28 @@ import javax.swing.JFrame;
 
 public class main extends JFrame implements KeyListener {
 
+	private static final long serialVersionUID = 1L;
 	private int anchoVentana=600;
-	private int altoVentana=400;
+	private int altoVentana=500;
 	
 	private int anchoRaqueta=60;
 	private int altoRaqueta=15;
 	private int velRaqueta=7;
 	
-	private int velPelotaX=5;
-	private int velPelotaY=5;
+	private int velPelota=6;
+	private int velPelotaX=0;
+	private int velPelotaY = (int) Math.sqrt((Math.pow(velPelota,2) - Math.pow(velPelotaX,2)));
 	private int ladoPelota=20;
 	
 	private int anchoBloque=40;
 	private int altoBloque=15;
 
-	private pelota pelota = new pelota(anchoVentana/2-ladoPelota/2, (altoVentana-ladoPelota)-altoRaqueta, velPelotaX, -velPelotaY, ladoPelota);
-	//private pelota pelota = new pelota(550, 50, velPelotaX, -velPelotaY, ladoPelota);
+	private pelota pelota = new pelota(anchoVentana/2-ladoPelota/2, (altoVentana-ladoPelota)-altoRaqueta-5, velPelota, velPelotaX, -velPelotaY, ladoPelota);
 	private raqueta raqueta = new raqueta(anchoVentana/2-anchoRaqueta/2, altoRaqueta, anchoRaqueta, velRaqueta);
 	
 	private int key=0;
 	private long tiempoDeRun;
-	private long tiempoDeSleep=20;
+	private long tiempoDeSleep=15;
 	private boolean run = false;
 	private ArrayList <bloque> arrayBloques=new ArrayList <bloque> ();
 	
@@ -40,7 +40,6 @@ public class main extends JFrame implements KeyListener {
 		new main();
 	}
 
-	
 	public main(){
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setSize(anchoVentana, altoVentana);
@@ -73,48 +72,67 @@ public class main extends JFrame implements KeyListener {
 	}
 	
     private void crearBloques(){
-    	int i = 0;
-    	
-    		for(int y=60;y<250; y+= 20){
+    	for(int y=60;y<250; y+= 25){
     			for (int x=30; x<anchoVentana-50; x+= 50){
     				arrayBloques.add(new bloque(x, y, altoBloque, anchoBloque, false));
-    				i++;
     			}
     		}
-    	i=0;
     	}
 	
 	private void calcularColisiones() {
 		
 		//Colision de la pelota contra la pared superior
-		if (pelota.y <=25){
+		if (pelota.y <=0){
     		pelota.veloY = -pelota.veloY;
     	}
 		//Colision de la pelota contra las paredes laterales
-		if (pelota.x <=0 || pelota.x >= anchoVentana-pelota.ladoPelota){
+		if (pelota.x <= 0 || pelota.x >= anchoVentana-pelota.ladoPelota){
     		pelota.veloX = -pelota.veloX;
     	}
 
 		//Colision de la bola con la raqueta
-    	if (pelota.x >= raqueta.x && pelota.x <= raqueta.x + raqueta.ancho && pelota.y >= altoVentana - raqueta.alto -20 && pelota.y <= altoVentana )
-    		pelota.veloY = -pelota.veloY;
-    	
-    	//colision de los bloques
+    	if (pelota.x >= raqueta.x && pelota.x <= raqueta.x + raqueta.ancho && pelota.y >= altoVentana - raqueta.alto -20 &&
+    	pelota.y <= altoVentana && pelota.veloY > 0) {
+    		pelota.veloX = pelota.veloX + ((pelota.x+pelota.ladoPelota/2)-(raqueta.x + raqueta.ancho/2))/10;
+    		if (pelota.veloX > 0 && pelota.veloX >= pelota.velo) {
+    			pelota.veloX = pelota.velo-1;
+    		}
+    		else if (pelota.veloX < 0  && pelota.veloX <= -pelota.velo) {
+    			pelota.veloX = -(pelota.velo-1);
+    		}
+    		if (pelota.veloY < 0) {
+    			pelota.veloY = (float) Math.sqrt((Math.pow(pelota.velo,2) - Math.pow(pelota.veloX,2)));
+    		} 
+    		else if (pelota.veloY > 0) {
+    			pelota.veloY = (float) -(Math.sqrt((Math.pow(pelota.velo,2) - Math.pow(pelota.veloX,2))));
+    		}
+    		
+    		
+    	}
+    		
+    	//Colision de los bloques
     	boolean colisionado = false;
         for (int i=0; i<arrayBloques.size(); i++) {
         	if (arrayBloques.get(i).destruido == false) {
-        		if ((pelota.x >= arrayBloques.get(i).x && pelota.x <= arrayBloques.get(i).x + arrayBloques.get(i).ancho || pelota.x + pelota.ladoPelota >= arrayBloques.get(i).x && pelota.x + pelota.ladoPelota<= arrayBloques.get(i).x + arrayBloques.get(i).ancho) 
-        				&& (pelota.y >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y <= arrayBloques.get(i).y || pelota.y + pelota.ladoPelota >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y + pelota.ladoPelota <= arrayBloques.get(i).y) ){
+        		
+        		if ((pelota.x >= arrayBloques.get(i).x && pelota.x <= arrayBloques.get(i).x + arrayBloques.get(i).ancho ||
+        		pelota.x + pelota.ladoPelota >= arrayBloques.get(i).x && pelota.x + pelota.ladoPelota<= arrayBloques.get(i).x + arrayBloques.get(i).ancho) 
+        		&& (pelota.y >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y <= arrayBloques.get(i).y ||
+        		pelota.y + pelota.ladoPelota >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y + pelota.ladoPelota <= arrayBloques.get(i).y) ) {
         			
-        			if (pelota.x >= arrayBloques.get(i).x && pelota.x <= arrayBloques.get(i).x + arrayBloques.get(i).ancho || pelota.x + pelota.ladoPelota >= arrayBloques.get(i).x && pelota.x + pelota.ladoPelota<= arrayBloques.get(i).x + arrayBloques.get(i).ancho && colisionado==false){
-        				pelota.veloY = -pelota.veloY;
-        				colisionado=true;
-        			}
-        			if (pelota.y >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y <= arrayBloques.get(i).y ||pelota.y + pelota.ladoPelota >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y + pelota.ladoPelota <= arrayBloques.get(i).y && colisionado==false) {
-        				pelota.veloX = -pelota.veloX;
-        				colisionado=true;
-        			}
-        			arrayBloques.get(i).destruido = true;
+	        			if (pelota.x >= arrayBloques.get(i).x && pelota.x <= arrayBloques.get(i).x + arrayBloques.get(i).ancho ||
+	        			pelota.x + pelota.ladoPelota >= arrayBloques.get(i).x && pelota.x + pelota.ladoPelota<= arrayBloques.get(i).x + arrayBloques.get(i).ancho &&
+	        			colisionado==false) {
+	        				pelota.veloY = -pelota.veloY;
+	        				colisionado=true;
+	        			}
+	        			if (pelota.y >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y <= arrayBloques.get(i).y ||
+	        			pelota.y + pelota.ladoPelota >= arrayBloques.get(i).y- pelota.ladoPelota && pelota.y + pelota.ladoPelota <= arrayBloques.get(i).y &&
+	        			colisionado==false) {
+	        				pelota.veloX = -pelota.veloX;
+	        				colisionado=true;
+	        			}
+	        			arrayBloques.get(i).destruido = true;
         		}
         	}
         }
@@ -127,19 +145,20 @@ public class main extends JFrame implements KeyListener {
 	}
 	
 	private void desplazamientoRaqueta() {
-		if(raqueta.x>25 && key==KeyEvent.VK_UP) 
-    		raqueta.x -= raqueta.velPaleta;
-    	if(raqueta.x <anchoVentana-raqueta.ancho && key==KeyEvent.VK_DOWN) 
+		if (raqueta.x > 0 && key==KeyEvent.VK_LEFT) {
+			raqueta.x -= raqueta.velPaleta;
+		}
+		else if (raqueta.x < anchoVentana-raqueta.ancho && key==KeyEvent.VK_RIGHT) {
     		raqueta.x += raqueta.velPaleta;
+		}
 	}
 	
 	private void reiniciarJuego() {
-		if (pelota.y > altoVentana){
+		if (pelota.y > altoVentana) {
     		pelota.x = anchoVentana/2 - ladoPelota/2;
     		pelota.y = altoVentana - ladoPelota - raqueta.alto;
     		raqueta.x = anchoVentana/2 - anchoRaqueta/2;
     		arrayBloques.clear();
-    		
     		run=false;
     	}
 	}
@@ -187,20 +206,17 @@ public class main extends JFrame implements KeyListener {
     
 	@Override
 	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
 		key=e.getKeyCode();
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
 		 key=0;
 	}
 
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
--- src/pelota.java
+++ src/pelota.java
@@ -1,13 +1,15 @@
 public class pelota {
     public int x;
     public int y;
-    public int veloX;
-    public int veloY;
+    public float velo;
+    public float veloX;
+    public float veloY;
     public int ladoPelota;
    
-    public pelota(int x, int y, int veloX, int veloY, int ladoPelota) {
+    public pelota(int x, int y, float velo, float veloX, float veloY, int ladoPelota) {
         this.x = x;
-        this.y = y; 
+        this.y = y;
+        this.velo = velo;
         this.veloX = veloX;
         this.veloY = veloY;
         this.ladoPelota= ladoPelota;
